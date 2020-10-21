56b15d59fe8b83f7e9817dfff6613e406567bd7a
Rohit Gupta <rohit_1290@yahoo.com>
2020-10-21 21:46:34 +0530

    update: capture spam for contact form

--- classes/SpamAnalysis.php
+++ classes/SpamAnalysis.php
@@ -10,33 +10,40 @@ class SpamAnalysis extends DefaultPluginBootstrap {
 	 */
 	public function init() {
 		
-    // register hooks
+    // register hooks - register
   	elgg_register_plugin_hook_handler("action:validate", "register", function(\Elgg\Hook $hook) {
     
       if(elgg_is_active_plugin('spam_login_filter')) {
-				// error_log("action:validate called");
 				$email = get_input('email');
-				$ip = get_ip();
+				$ip = \Spam\LoginFilter\get_ip();
         if (!\Spam\LoginFilter\check_spammer($email, $ip)) {
-					// error_log("action:validate check_spammer called");
           save_spam_log_to_txt($email, $ip, "action:validate-register");
         }
       }
-
       return $hook->getValue();
-
+    });
+    
+		// register hooks - send_mail
+  	elgg_register_plugin_hook_handler("action:validate", "send_mail", function(\Elgg\Hook $hook) {
+    
+      if(elgg_is_active_plugin('spam_login_filter')) {
+				$ip = \Spam\LoginFilter\get_ip();
+				$email = get_input('email');
+        if (!\Spam\LoginFilter\check_spammer($email, $ip)) {
+          save_spam_log_to_txt($email, $ip, "action:validate-send_mail");
+        }
+      }
+      return $hook->getValue();
     });
     
 		/*
     elgg_register_plugin_hook_handler('register', 'user', function(\Elgg\Hook $hook) {
 
       if(elgg_is_active_plugin('spam_login_filter')) {
-				// error_log("register:user called");
         $p = $hook->getParams();
         $email = $p['user']->email;
         $ip = get_ip();
         if (!\Spam\LoginFilter\check_spammer($email, $ip)) {
-					// error_log("register:user check_spammer called");
           save_spam_log_to_txt($email, $ip, "action:validate-register");
         }
       }
@@ -44,9 +51,11 @@ class SpamAnalysis extends DefaultPluginBootstrap {
       return $hook->getValue();
     });
 		*/
-		
+			
+		// plugin hook cron daily
 		elgg_register_plugin_hook_handler('cron', 'daily', 'remove_old_log_files');
 
+		// Administer Utilities Menu
   	elgg_register_menu_item('page', [
   		'name' => 'managespam',
   		'href' => 'admin/administer_utilities/managespam',
